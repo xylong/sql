649e9c819e94773922959358a6e2c8508c4de8e5
xyl <416319808@qq.com>
2024-05-15 00:38:40 +0800

    创建用户表

---
+++ example.sql
@@ -0,0 +1,34 @@
+CREATE TABLE `users` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '?',
+  `phone` char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '手机号',
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '邮箱',
+  `wechat_unionid` varchar(50) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '微信unionid',
+  `wechat_applet_openid` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '微信小程序id',
+  `wechat_official_openid` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '微信公众号id',
+  `name` varchar(15) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '姓名',
+  `nickname` varchar(30) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '昵称',
+  `avatar` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '头像',
+  `created_at` timestamp NOT NULL COMMENT '创建时间',
+  `updated_at` timestamp NOT NULL COMMENT '更新时间',
+  `deleted_at` timestamp NULL DEFAULT NULL COMMENT '删除时间',
+  PRIMARY KEY (`id` DESC) USING BTREE,
+  UNIQUE KEY `idx_phone` (`phone` DESC) USING BTREE COMMENT '手机号索引',
+  UNIQUE KEY `idx_email` (`email` DESC) USING BTREE COMMENT '邮箱索引'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `user_profiles` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `user_id` int unsigned NOT NULL DEFAULT '0' COMMENT '用户id',
+  `gender` tinyint(1) NOT NULL COMMENT '性别：0保密 1 男 2女 3未知',
+  `level` tinyint(1) NOT NULL DEFAULT '0' COMMENT '等级',
+  `password` char(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '密码',
+  `salt` char(30) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '盐',
+  `birthday` date DEFAULT NULL COMMENT '出生日期',
+  `signature` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '个性签名',
+  `created_at` timestamp NOT NULL COMMENT '创建时间',
+  `updated_at` timestamp NOT NULL COMMENT '更新时间',
+  `deleted_at` timestamp NULL DEFAULT NULL COMMENT '删除时间',
+  PRIMARY KEY (`id` DESC),
+  UNIQUE KEY `idx_user` (`user_id`) USING BTREE COMMENT '用户id索引'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
